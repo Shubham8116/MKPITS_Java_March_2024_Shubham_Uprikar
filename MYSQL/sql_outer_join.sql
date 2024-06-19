04b0dc6db622cb4f2fe79a28a2c506bd25b56a61
Shubham <suprikar5@gmail.com>
2024-06-19 17:32:18 +0530

    sql programs

--- MYSQL/sql_outer_join.sql
+++ MYSQL/sql_outer_join.sql
@@ -4,6 +4,7 @@
 
 #Problem Statement 1: Retrieve Customer Information with Rental History
 #Retrieve the customer's first name, last name, and the number of rentals they have made. Include customers who have not made any rentals as well.
+
 select c.first_name,c.last_name,count(rental_id) from customer c left join
 rental r on c.customer_id=r.customer_id group by c.first_name,c.last_name order by c.customer_id;
 
@@ -18,12 +19,14 @@ rental r on i.inventory_id=r.inventory_id group by f.title order by f.title;
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Problem Statement 3: List Customers and Their Rentals
 #List customers with their rental details (rental ID, rental date, and return date if available). Include customers who have not made any rentals.
+
 select c.first_name,c.last_name,r.rental_id,r.rental_date,r.return_date from customer c  left join
 rental r on c.customer_id=r.customer_id group by c.first_name,c.last_name order by c.customer_id;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Problem Statement 4: Show Films and Customers Who Rented Them
 #Display film titles and the names of customers who have rented each film. Include films that have not been rented.
+
 select f.title,c.customer_id,c.first_name,c.last_name from film f left join
 inventory i on f.film_id=i.film_id join
 rental r on i.inventory_id=r.inventory_id join
@@ -32,13 +35,14 @@ customer c on r.customer_id=c.customer_id group by f.title,c.customer_id,c.first
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Problem Statement 5: Retrieve Films and Rental Details
 #Retrieve film titles and rental details (rental ID, rental date, and return date if available). Include films that have not been rented.
+
 select f.title,r.rental_id,r.rental_date,r.return_date from film f left join
 inventory i on f.film_id=i.film_id join
 rental r on i.inventory_id=r.inventory_id group by f.title,rental_id ;
 
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Problem Statement 8: Show Actors and Films They Acted In
+#Problem Statement 6: Show Actors and Films They Acted In
 #Display actor names and the titles of films they have acted in. Include actors who have not acted in any films.
 
 select a.actor_id,a.first_name,a.last_name,f.title from actor a left join
@@ -46,8 +50,9 @@ film_actor fa on a.actor_id=fa.actor_id join
 film f on fa.film_id=f.film_id  order by a.actor_id;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Problem Statement 9: Explore Staff and Their Associated Stores
+#Problem Statement 7: Explore Staff and Their Associated Stores
 #Retrieve staff details (staff ID, first name, last name) and the store they are associated with. Include staff who are not associated with any store.
+
 select s.staff_id,s.first_name,s.last_name,st.store_id from staff s
 left join store st on s.store_id = st.store_id
 group by s.staff_id
